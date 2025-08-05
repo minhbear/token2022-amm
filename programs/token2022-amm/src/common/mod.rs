b9e9bee2465a5786f99dd511370472c9a615539a
minhbear <minhbeardev@gmail.com>
2025-08-06 01:00:51 +0700

    feat: init

---
+++ programs/token2022-amm/src/common/mod.rs
@@ -0,0 +1,8 @@
+pub mod constant;
+pub use constant::*;
+
+pub mod event;
+pub use event::*;
+
+pub mod error;
+pub use error::*;
--- programs/token2022-amm/src/lib.rs
+++ programs/token2022-amm/src/lib.rs
@@ -1,16 +1,13 @@
 use anchor_lang::prelude::*;
 
+mod common;
+mod instructions;
+mod state;
+mod utils;
+
 declare_id!("J4a3aueEwbNTW84q6DnEvVZG2EC182fvnXYSnoE6zDkF");
 
 #[program]
 pub mod token2022_amm {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+  use super::*;
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
